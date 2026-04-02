cc9358b7b765e095011103ea4c033f534b531c75
ShivaniFullStactDeveloper <156500244+ShivaniFullStactDeveloper@users.noreply.github.com>
2026-04-02 10:42:00 +0530

    Add files via upload

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,21 +1,22 @@
 import pkg from "pg";
 import dotenv from "dotenv";
-// Load environment variables from .env file
+
 dotenv.config();
 
 const { Pool } = pkg;
-// Create a new pool instance with the database configuration
+
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+
+  //  Important for Render PostgreSQL
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 // Test connection
 pool.connect()
   .then(() => console.log(" DB Connected"))
-  .catch((err) => console.error("DB Error:", err.message));
+  .catch((err) => console.error(" DB Error:", err.message));
 
 export default pool;
